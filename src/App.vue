81de1fd78cee3355e6d92759e4177ce5322ba978
KwonYG <andamirocll@gmail.com>
2019-09-26 00:18:30 +0900

    Add: AppHeader.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <app-header></app-header>
+    <router-view />
   </div>
 </template>
-
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import AppHeader from "@/components/AppHeader.vue";
+@Component({
+  components: { AppHeader }
+})
+export default class App extends Vue {}
+</script>
 <style>
+html,
+body {
+  margin: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  font-family: "Roboto Mono", monospace;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,81 @@
+<template>
+  <header class="app_header">
+    <nav>
+      <ul class="menu_list">
+        <li class="list_item menu">
+          <span>KwonYG</span>
+        </li>
+        <li class="list_item menu">
+          <span>설정</span>
+        </li>
+        <li class="list_item menu">
+          <span>도움말</span>
+        </li>
+      </ul>
+    </nav>
+    <aside class="side_display">
+      <span class="display_item clock">{{currentTime}}</span>
+    </aside>
+  </header>
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class AppHeader extends Vue {
+  currentTime: string = "";
+
+  refreshTime() {
+    const date = new Date();
+    const month = date.getMonth();
+    const clockDate = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    this.currentTime =
+      `${month + 1}월 ${clockDate}일 ` +
+      `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  }
+
+  mounted() {
+    setInterval(this.refreshTime, 1000);
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background-color: #ecfbfe;
+}
+
+ul,
+li {
+  margin: 0;
+  padding: 0;
+}
+
+.app_header {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid black;
+}
+
+nav .menu_list {
+  display: flex;
+}
+
+nav .menu_list .list_item {
+  margin-left: 5px;
+}
+
+nav .menu_list .list_item span {
+  cursor: pointer;
+}
+
+aside .display_item {
+  padding: 0 6px;
+  border-left: 1px solid black;
+}
+</style>
